96e213de05855102c7be4fc898e9de491c27f0fb
laZuR1s <sashalazyr@gmail.com>
2024-10-15 15:59:08 +0300

    another small fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from cmath import polar
 from random import randint
 
@@ -5,18 +6,30 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+# Автор: Лазарев А.А.
 
+# Цвет: любой который можно задать одной буквой (но для каждого свой).
+# Линия: сплошная, пунктирная, сплошная, пунктирная.
+# Толщина: 2, 3, 2, 3.
+# Сетка: ☒,☑,☑,☑.
+# Задать пользовательские значения вдоль оси x (θ или φ)
 
-plt.figure("Yoi", figsize=(12, 7))
+# (A) 𝑟 = sin 111𝜑/122
+# (B) Формулы заданы параметрически:
+# x = a·cos(t) y = a·sin(2·t)/2
+# и
+# x = b·cos(t) y = b·sin(2·t)/2
+# где a и b — случайные вещественные числа
+# из отрезка [1, 5]
 
+plt.figure("Yoi", figsize=(15, 10))
 
-angles = np.linspace(0, 2 * np.pi, 1000)
+angles = np.linspace(0, 20 * 2 * np.pi, 1000)
 
-r1= np.sin(111/122 * angles)
+r1 = np.sin(111 / 122 * angles)
 
-
-first = plt.subplot2grid((2, 2), (1, 0),polar=True)
-first.set_yticklabels([]);
+first = plt.subplot2grid((3, 3), (1, 0), polar=True)
+first.set_yticklabels([])
 
 first.plot(angles, r1, linewidth=2, linestyle='-', color='r')
 
@@ -31,7 +44,7 @@ y1 = a * np.sin(2 * t) / 2
 x2 = b * np.cos(t)
 y2 = b * np.sin(2 * t) / 2
 
-second = plt.subplot2grid((2, 2), (0, 1))
+second = plt.subplot2grid((3, 3), (0, 2))
 second.grid(True)
 
 second.plot(x1, y1, linewidth=3, linestyle='--', color='b')
